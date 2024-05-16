f4598c04f9da04b3f987e6cd01e11626661c3a95
khaledkamr <200013171@must.edu.eg>
2024-05-16 18:43:19 +0300

    Added comments

--- Maze solver.py
+++ Maze solver.py
@@ -1,27 +1,32 @@
 from pyamaze import maze, agent, COLOR, textLabel
 from queue import PriorityQueue
 
-def h(cell1, cell2):
-    x1, y1 = cell1
-    x2, y2 = cell2
-    return abs(x1-x2) + abs(y1-y2)
+# Heuristic function: Manhattan distance between the current cell and the goal cell:(1,1)
+def h(cell):
+    x, y = cell
+    return (x - 1) + (y - 1)
 
 def aStar(m):
+    # Start cell is the bottom-right corner of the maze
     start = (m.rows, m.cols)
+
+    # Initialize g_score and f_score for each cell in the maze
     g_score = {cell:float('inf') for cell in m.grid}
     g_score[start] = 0
     f_score = {cell:float('inf') for cell in m.grid}
-    f_score[start] = h(start, (1,1))
+    f_score[start] = h(start)
 
+    # Priority queue to store open cells, ordered by f_score
     open = PriorityQueue()
-    open.put((h(start, (1,1)), h(start, (1,1)), start))
+    open.put((f_score[start], h(start), start))
     aPath = {}
 
     while not open.empty():
         currCell = open.get()[2]
         if currCell == (1,1):
             break
-        
+
+        # Check adjacent cells
         for d in "ESNW":
             if m.maze_map[currCell][d] == True:
                 if d == 'E':
@@ -34,14 +39,16 @@ def aStar(m):
                     childCell = (currCell[0] + 1, currCell[1])
         
                 temp_g_score = g_score[currCell] + 1
-                temp_f_score = temp_g_score + h(childCell, (1,1))
+                temp_f_score = temp_g_score + h(childCell)
 
+                # Check if the new path of the adjacent cell is better
                 if temp_f_score < f_score[childCell]:
                     g_score[childCell] = temp_g_score
                     f_score[childCell] = temp_f_score
-                    open.put((temp_f_score, h(childCell, (1,1)), childCell))
+                    open.put((temp_f_score, h(childCell), childCell))
                     aPath[childCell] = currCell
     
+    # Reconstruct the forward path from start to target
     fwdPath = {}
     cell = (1,1)
     while cell != start:
@@ -50,7 +57,7 @@ def aStar(m):
 
     return fwdPath
 
-m = maze(10,10)
+m = maze(5,5)
 m.CreateMaze()
 path = aStar(m)
 a = agent(m, footprints=True, color=COLOR.blue, filled=True)
